4c881836c849ebab3c3834fbc83b9e19375f61ce
Crowdbotics <team@crowdbotics.com>
2020-04-09 06:36:48 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,6 +8,13 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    test = models.ForeignKey(
+        "home.HomePage",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_test",
+    )
 
     def __str__(self):
         return self.title
